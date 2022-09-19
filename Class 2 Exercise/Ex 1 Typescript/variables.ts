d447f80b8fc612518b3b7610300860bcf259d48c
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-09-19 14:46:26 -0500

    Delete Class 2 Exercise directory

--- Class 2 Exercise/Ex 1 Typescript/variables.ts
+++
@@ -1,5 +0,0 @@
-const nameOne: string = "whatever";
-const numberOne: number = 13;
-const bool2: boolean = true;
-let null2: null = null;
-let und2: void; 
--- Class 2 Exercise/Ex 2/main.js
+++
@@ -1,3 +0,0 @@
-const food1 = 45
-const food2 = 25
-const food3 = 30
