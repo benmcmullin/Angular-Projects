916b53a11612041da18ab7f9c14ee0113505a04a
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-09-19 14:46:09 -0500

    Delete Class 7 Exercise directory

--- Class 7 Exercise/Ex 1/main.js
+++
@@ -1,8 +0,0 @@
-function redundantReturn (str){
-    function strReturn () {
-        
-        console.log("Function Returned String: ", str);
-    }
-strReturn();
-}
-redundantReturn("Have a good day!");
--- Class 7 Exercise/Ex 3/main.js
+++
@@ -1,8 +0,0 @@
-drinks = [
-    { name: "lemonade", price: 50 },
-    { name: "lime", price: 10 },
-    { name: "tea", price: 15 },
-    { name: "water", price: 5}
-  ]
-  const drinkByPrice = drinks.sort((a, b) => a.price - b.price);
-  console.log("Drink Prices in Ascending Order: ", drinkByPrice);
